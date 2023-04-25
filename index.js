25bb50d8fa7cfd1f27cb81ae685f49ae798ab1f9
Abby2911 <124635236+Abby2911@users.noreply.github.com>
2023-04-25 14:24:52 +0000

    🌟 Built In Variables

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 // 1. Importar el modulo de node 
 // HTTP
 import http from 'http';
+//Biblioteca path
+import path from 'path';
+
+//Recreando Built-In Variables 
+global["__dirname"] = path.dirname(new URL(import.meta.url).pathname);
+global["__filename"] = path.join(__dirname, path.basename(new URL(import.meta.url).pathname));
 
 // 2. Crear el servidor 
-const server = http.createServer( (req, res) => {
+const server = http.createServer( (_, res) => {
     //Logica del servidor
     // 1. Respondiendo al Cliente
-    res.write("Hello from the server...");
+    res.write(`
+    __dirname: ${__dirname}
+    __filename: ${__filename}`);
+  
     // 2. Cerrar la conexion 
     res.end();
 } );
 
 // 3. Arrancamos el servidor 
 server.listen(3000, "0.0.0.0", () => {
-    console.log("Servidor escuchando en http://localhost3000");
+    console.log(" 🤩🤩 Servidor escuchando en http://localhost3000");
 } );
